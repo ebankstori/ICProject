4891884217147235720933aafd67576c9c0fbf68
teomilos <teodoramilos@icloud.com>
2023-11-28 11:20:03 -0400

    'DiceGame'

--- DiceGame.v
+++ DiceGame.v
@@ -2,47 +2,47 @@ module DiceGame(
     input clock,
 	 input reset, // input reset
     input roll, // input starting roll
-    output [6:0] disp1, //output first 7 segment display
-    output [6:0] disp2, // output second 7 segment display
-    output win, // output green LED for win
-    output lose // output red LED for lose
+    output [6:0] seg_disp1, //output first 7 segment display
+    output [6:0] seg_disp2, // output second 7 segment display
+    output win_1, // output green LED for win
+    output loss_1 // output red LED for lose
 );
 
-reg reg_disp1;
-reg reg_disp2;
+reg reg_seg_disp1;
+reg reg_seg_disp2;
 reg reg_win;
-reg reg_lose;
+reg reg_loss;
 
-assign disp1 = reg_disp1;
-assign disp2 = reg_disp2;
-assign win = reg_win;
-assign lose = reg_lose;
+assign seg_disp1 = reg_seg_disp1;
+assign seg_disp2 = reg_seg_disp2;
+assign win_1 = reg_win;
+assign loss_1 = reg_loss;
 
 fsm uut (
 	.clock(clock),
-	.win(win),
-	.lose(lose),
+	.win(reg_win),
+	.loss(reg_loss),
 	.reset(reset),
 	.roll(roll),
-	.disp1(disp1),
-	.disp2(disp2)
+	.disp1(reg_seg_disp1),
+	.disp2(reg_seg_disp2)
 );
 
     always @(posedge clock) begin
         // display values only when the enter/roll button is released
         if (~roll) begin
-            //disp1 <= win;
-            //disp2 <= lose;
-				reg_disp1 <= win ? 7'b1000000 : 7'b0000000; // Assuming '1' lights up the segment
-            reg_disp2 <= lose ? 7'b1000000 : 7'b0000000; // Assuming '1' lights up the segment
+				reg_seg_disp1 <= disp1;
+            reg_seg_disp2 <= disp2; 
         end
     end
 // outside of always block put assign and net (output) = register, and then have seperate register 
     always @(posedge clock) begin
         // reset
         if (reset) begin
+				reg_win <= win;
+				reg_loss <= loss;
             reg_win <= 0;
-            reg_lose <= 0;
+            reg_loss <= 0;
         end
     end
 
