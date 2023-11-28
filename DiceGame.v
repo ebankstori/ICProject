c207a6a31a151157b2ab562313abfdcf591f355a
teomilos <teodoramilos@icloud.com>
2023-11-28 11:00:44 -0400

    DiceGame

--- DiceGame.v
+++ DiceGame.v
@@ -1,72 +1,49 @@
-module DiceGame(starting_roll, reset, clk, rst, segdisp_1, segdisp_2, win, lose, dice1_out, dice2_out);
+module DiceGame(
+    input clock,
+	 input reset, // input reset
+    input roll, // input starting roll
+    output [6:0] disp1, //output first 7 segment display
+    output [6:0] disp2, // output second 7 segment display
+    output win, // output green LED for win
+    output lose // output red LED for lose
+);
 
-  input starting_roll; // dice roll input signal
-  input reset; // input reset for game state
-  input clk; // clock input
-  input rst; // reset input for counters
-  output reg [6:0] segdisp_1; // output for first 7-seg display
-  output reg [6:0] segdisp_2; // output for second 7-seg display
-  output reg win; // output - indicates player has won
-  output reg lose; // output - indicated player has lost
-  output reg [2:0] dice1_out; // output - vaue of die 1
-  output reg [2:0] dice2_out; // output - value of die 2
+reg reg_disp1;
+reg reg_disp2;
+reg reg_win;
+reg reg_lose;
 
+assign disp1 = reg_disp1;
+assign disp2 = reg_disp2;
+assign win = reg_win;
+assign lose = reg_lose;
 
-  reg [2:0] dice1_counter; // register representing value of die 1
-  reg [2:0] dice2_counter; // register representing value of die 2
-  reg rolling; // register to indicate the dice are currently rolling
+fsm uut (
+	.clock(clock),
+	.win(win),
+	.lose(lose),
+	.reset(reset),
+	.roll(roll),
+	.disp1(disp1),
+	.disp2(disp2)
+);
 
-  // instantiate the counters module
-  counters uut (
-    .clk(clk),
-    .rst(rst),
-    .dice1_out(),
-    .dice2_out()
-  );
-
-  always @(posedge reset or posedge starting_roll)
-  // trigger on the + clock edge or + edge of rst signal
-  begin
-    if (reset)
-	 // on a + edge of reset, variables are initialzied
-    begin
-      dice1_counter <= 3'b000;
-      dice2_counter <= 3'b000;
-      segdisp_1 <= 7'b0000000;
-      segdisp_2 <= 7'b0000000;
-      rolling <= 1'b0;
-      win <= 1'b0;
-      lose <= 1'b0;
+    always @(posedge clock) begin
+        // display values only when the enter/roll button is released
+        if (~roll) begin
+            //disp1 <= win;
+            //disp2 <= lose;
+				reg_disp1 <= win ? 7'b1000000 : 7'b0000000; // Assuming '1' lights up the segment
+            reg_disp2 <= lose ? 7'b1000000 : 7'b0000000; // Assuming '1' lights up the segment
+        end
     end
-    else if (starting_roll)
-	 // on a + edge of starting_roll, flag is set to rolling
-    begin
-      rolling <= 1'b1;
+// outside of always block put assign and net (output) = register, and then have seperate register 
+    always @(posedge clock) begin
+        // reset
+        if (reset) begin
+            reg_win <= 0;
+            reg_lose <= 0;
+        end
     end
-    else if (rolling)
-    begin
-      // if rolling, the values of dice are displated on 7-seg displays
-      segdisp_1 <= dice1_counter;
-      segdisp_2 <= dice2_counter;
-    end
-    else
-    begin
-      // check if the player has won or lost
-      case (dice1_counter + dice2_counter)
-        4'b0011: lose <= 1'b1; // sum is 2
-        4'b0100: lose <= 1'b1; // sum is 3
-        4'b0110: lose <= 1'b1; // sum is 4
-        4'b0111: lose <= 1'b1; // sum is 5
-        4'b1000: lose <= 1'b1; // sum is 6
-		  4'b0101: win <= 1'b1;  // sum is 7
-        4'b1010: lose <= 1'b1; // sum is 8
-        4'b1011: lose <= 1'b1; // sum is 9
-        4'b1100: lose <= 1'b1; // sum is 10
-		  4'b1001: win <= 1'b1;  // sum is 11
-        4'b1101: win <= 1'b1;  // sum is 12
-        default: rolling <= 1'b0; // continue rolling
-      endcase
-    end
-  end
 
 endmodule
--- testbench.v
+++ testbench.v
@@ -1,91 +1,46 @@
-`timescale 1ns/1ns
-
 module testbench;
+    reg clock;
+    reg reset;
+    reg roll;
+    wire [6:0] disp1;
+    wire [6:0] disp2;
+    wire win;
+    wire lose;
 
-  parameter period = 10; // clock period in ns
+    // Instantiate the DiceGame module
+    DiceGame uut (
+        .clock(clock),
+        .reset(reset),
+        .roll(roll),
+        .disp1(disp1),
+        .disp2(disp2),
+        .win(win),
+        .lose(lose)
+    );
 
-  reg clk; // input clock signal
-  reg rst; // input reset signal 
-  reg starting_roll; //input starting_roll signal
-  wire [2:0] dice1_out; // output from dice 1 
-  wire [2:0] dice2_out; // output from dice 2
+    // Clock generation
+    initial begin
+        clock = 0;
+        forever #5 clock = ~clock;
+    end
 
-  wire [6:0] segdisp_1; // first 7-seg display output
-  wire [6:0] segdisp_2; // second 7-seg display output
-  wire win; // win output
-  wire lose; // lose output
+    
+    initial begin
+        reset = 1;
+        roll = 0;
 
+        #10 reset = 0;
 
-  // instantiate DiceGame module
-  DiceGame cct (
-    .starting_roll(starting_roll),
-    .reset(rst),
-    .clk(clk),
-    .rst(rst),
-    .segdisp_1(segdisp_1),
-    .segdisp_2(segdisp_2),
-    .win(win),
-    .lose(lose),
-    .dice1_out(dice1_out),
-    .dice2_out(dice2_out)
-  );
+        #10 roll = 1;
 
-  // instantiate counters module
-  counters uut (
-    .clk(clk),
-    .rst(rst),
-    .dice1_out(dice1_out),
-    .dice2_out(dice2_out)
-  );
+        #100;
 
-  // generate a clock
-  always #((period / 2)) clk = ~clk;
+        roll = 0;
 
+        #100;
 
-  initial begin
-  // initialize values for clk, rst, starting_roll
-    clk = 0;
-    rst = 1;
-    starting_roll = 0;
+        reset = 1;
 
-    // apply reset after 10 ns
-    #10 rst = 0;
-
-		// Test case 1: Normal game
-    #20 starting_roll = 1;
-    #30 starting_roll = 0; // Allow some time for counters to increment
-    #40 starting_roll = 1;
-    #50 starting_roll = 0; // Allow some time for counters to increment
-    #60 starting_roll = 1;
-    #70 starting_roll = 0; // Allow some time for counters to increment
-    #80 starting_roll = 1;
-    #90 starting_roll = 0; // Allow some time for counters to increment
-
-    // Test case 2: Win condition
-    #10 rst = 1;
-    #20 starting_roll = 1;
-    #30 starting_roll = 0; // Allow some time for counters to increment
-    #40 starting_roll = 1;
-    #50 starting_roll = 0; // Allow some time for counters to increment
-    #60 starting_roll = 1;
-    #70 starting_roll = 0; // Allow some time for counters to increment
-    #80 starting_roll = 1;
-    #90 starting_roll = 0; // Allow some time for counters to increment
-
-    // Test case 3: Lose condition  
-    #10 rst = 1;
-    #20 starting_roll = 1;
-    #30 starting_roll = 0; // Allow some time for counters to increment
-    #40 starting_roll = 1;
-    #50 starting_roll = 0; // Allow some time for counters to increment
-    #60 starting_roll = 1;
-    #70 starting_roll = 0; // Allow some time for counters to increment
-    #80 starting_roll = 1;
-    #90 starting_roll = 0; // Allow some time for counters to increment
-
-
-    #2000 $finish;
-  end
-
+        #10 $finish;
+    end
 endmodule
-
